e5bc7d1056ddfb2092d5e17fc31e76153da3e5f5
XIA <alexandre.xia@etu.sorbonne-universite.fr>
2024-01-29 16:07:34 +0100

    gc some tests

--- GraphModel.py
+++ GraphModel.py
@@ -28,7 +28,13 @@ from torch.utils.data import DataLoader, Dataset
 from sklearn.metrics import accuracy_score
 import torchmetrics as tm
 from tqdm import notebook, tqdm
+from sklearn.metrics import RocCurveDisplay, roc_curve
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
+"""
+Cette version utilise les donnees fournies par les auteurs, les matrices d'incidences sont normalisees ce qui donne de la stabilité. Cependant,
+ils ont signale dans l'article que dans certains cas, on perdait l'injectivite (certaines donnees seraient similaires ce qui causeraient des erreurs.)
+"""
 # Chargement des donnees ATTENTION, les donnees necesitte dlg version 0.9.1 les versions recentes causeront une erreur
 path = "./gc/proteins"
 training_graphs = np.load(path+'/training_graphs_concat_.npy',allow_pickle=True)
@@ -72,24 +78,25 @@ x2_0_val = np.load(path+'/x2_0_val_concat_.npy',allow_pickle=True)
 x2_1_val = np.load(path+'/x2_1_val_concat_.npy',allow_pickle=True)
 x2_2_val = np.load(path+'/x2_2_val_concat_.npy',allow_pickle=True)
 
-# x0_0_test = np.load(path+'/x0_0_test_concat_.npy',allow_pickle=True)
-# x0_1_test = np.load(path+'/x0_1_test_concat_.npy',allow_pickle=True)
-# x0_2_test = np.load(path+'/x0_2_test_concat_.npy',allow_pickle=True)
-# x1_0_test = np.load(path+'/x1_0_test_concat_.npy',allow_pickle=True)
-# x1_1_test = np.load(path+'/x1_1_test_concat_.npy',allow_pickle=True)
-# x1_2_test = np.load(path+'/x1_2_test_concat_.npy',allow_pickle=True)
-# x2_0_test = np.load(path+'/x2_0_test_concat_.npy',allow_pickle=True)
+x0_0_test = np.load(path+'/x0_0_test_concat_.npy',allow_pickle=True)
+x0_1_test = np.load(path+'/x0_1_test_concat_.npy',allow_pickle=True)
+x0_2_test = np.load(path+'/x0_2_test_concat_.npy',allow_pickle=True)
+x1_0_test = np.load(path+'/x1_0_test_concat_.npy',allow_pickle=True)
+x1_1_test = np.load(path+'/x1_1_test_concat_.npy',allow_pickle=True)
+x1_2_test = np.load(path+'/x1_2_test_concat_.npy',allow_pickle=True)
+x2_0_test = np.load(path+'/x2_0_test_concat_.npy',allow_pickle=True)
 
-# x2_1_test = np.load(path+'/x2_1_test_concat_.npy',allow_pickle=True)
-# x2_2_test = np.load(path+'/x2_2_test_concat_.npy',allow_pickle=True)
+x2_1_test = np.load(path+'/x2_1_test_concat_.npy',allow_pickle=True)
+x2_2_test = np.load(path+'/x2_2_test_concat_.npy',allow_pickle=True)
 
 # Essai de fonction precomputing
 
 # L'auteur de l'article retrouvee ! -> une video de presentation par lui meme : Simple Yet Powerful Graph-aware and Simplicial-aware Neural Models
+# 
 def testprecomputing(bk1,bk2,bk3,xk1,xk2,xk3):
     """
-    bk1 : matrice incidence Nk-2 x Nk-1
-    bk2 : matrice incidence Nk-1 x Nk <- ce parametre pose un soucis de dimension
+    bk1 : matrice incidence Nk-2 x Nk-1 -> inutile mais dans l'article
+    bk2 : matrice incidence Nk-1 x Nk 
     bk3 : matrice incidence Nk x Nk+1
     xk2 : le vecteur entree (soit un one hot cf. Errica, soit un vecteur de 1 d'apres l'article )
     xk1 et xk3 : idem
@@ -123,9 +130,10 @@ def testprecomputing(bk1,bk2,bk3,xk1,xk2,xk3):
     # nouvelle dim : Nk x Nk+1
     ykc = akc@xk3
 
-    print(yku.shape,ykl.shape,ykb.shape,ykc.shape)
+    #print(yku.shape,ykl.shape,ykb.shape,ykc.shape)
     return np.hstack((yku,ykl,ykb,ykc))
     #return np.concatenate((yku,ykl,ykc))
+
 
 
 # matrice d'incidence en O((Nk-1 x Nk)**2) : polynomiale
@@ -153,7 +161,7 @@ def makeIncidence(edges, threeclique):
 # Le modele utilise pour la tache de classification de graphe
 class Model(nn.Module):
     # indim : dimension pour les donnees d'entree
-    def __init__(self,d1,d2,d3,d4,d5,d6,d7,d8,n_c, activation = nn.LeakyReLU, sortie = nn.Sigmoid):
+    def __init__(self,d1,d2,d3,d4,n_c, activation = nn.LeakyReLU, sortie = nn.Sigmoid):
         super(Model,self).__init__()
 
         self.act = activation()
@@ -185,7 +193,7 @@ class Model(nn.Module):
         self.g2_2 = nn.Sequential(nn.Linear(30,d2),self.act,nn.Linear(d2,d3),self.act,nn.Linear(d3,d3),self.act,nn.Linear(d3,d3),self.act)
 
         #Un mlp tres basique
-        self.D = nn.Sequential(nn.Linear(3*3*d3,d8),self.act,nn.Linear(d8,d8),self.act,nn.Linear(d8,d8),self.act,nn.Linear(d8,n_c),sortie()) #nn.Softmax(dim=0) for multi-class
+        self.D = nn.Sequential(nn.Linear(3*3*d3,d4),self.act,nn.Linear(d4,d4),self.act,nn.Linear(d4,d4),self.act,nn.Linear(d4,n_c),sortie()) #nn.Softmax(dim=0) for multi-class
     
     def forward(self, x0_0, x0_1, x0_2, x1_0, x1_1, x1_2, x2_0, x2_1, x2_2):
         # Learning From simplicial aware features
@@ -217,16 +225,16 @@ class Model(nn.Module):
 #print(x0_0_tr[0][0])
 #print(x1_0_tr[0][0])
 #print(x2_0_tr[0][0])
-options = {
-    'node_color': 'black',
-    'node_size': 20,
-    'width': 1,
-}
+# options = {
+#     'node_color': 'black',
+#     'node_size': 20,
+#     'width': 1,
+# }
 
 # Pour afficher un graphe
 #print(training_graphs[0][0])
-G = dgl.to_networkx(training_graphs[0][0])
-undirected = G.to_undirected() # On passe en non oriente car l'article le fait comme ca
+#G = dgl.to_networkx(training_graphs[0][0])
+#undirected = G.to_undirected() # On passe en non oriente car l'article le fait comme ca
 #print("Les aretes ", len(undirected.edges))
 #print(list(G.nodes(data=True))) # les atomes ne sont pas dans le dataset fourni (on prendra tel quel pour eviter de perdre du temps)
 #plt.figure(figsize=[15,7])
@@ -235,24 +243,24 @@ undirected = G.to_undirected() # On passe en non oriente car l'article le fait c
 #plt.show()
 
 # Trouver toutes les 3-cliques (2-simplex) avec networkx
-all_cliques= nx.enumerate_all_cliques(undirected)
-triad_cliques=[x for x in all_cliques if len(x)==3 ]
+#all_cliques= nx.enumerate_all_cliques(undirected)
+#triad_cliques=[x for x in all_cliques if len(x)==3 ]
 #print(triad_cliques)
 
 # 2-clique = aretes
 
-twoclique = undirected.edges()
+#twoclique = undirected.edges()
 
-oneclique = undirected.nodes()
+#oneclique = undirected.nodes()
 #print(twoclique)
 
 # Matrice d'incidencee b1 (taille N0 x N1)
-b1 = nx.incidence_matrix(undirected,oneclique,twoclique).todense()
+#b1 = nx.incidence_matrix(undirected,oneclique,twoclique).todense()
 # on essaie de faire le calcul 
 
 # Matrice d'incidence b2 (taille N1 x N2)
 #print(x0_0_tr[0][0])
-b2 = makeIncidence(twoclique,triad_cliques)
+#b2 = makeIncidence(twoclique,triad_cliques)
 
 #ak1 = b2@b2.T
 #ykl = ak1@x1_0_tr[0][0]
@@ -264,10 +272,10 @@ b2 = makeIncidence(twoclique,triad_cliques)
 #print(x1_1_tr[0][0][0])
 
 # On test pour k = 1
-newx11 = testprecomputing(None,b1,b2,x0_0_tr[0][0],x1_0_tr[0][0],x2_0_tr[0][0])
-# le precomputing ne redonne pas le meme resultat
-print("nos resultats ",newx11)
-print("leurs resultats ", x1_1_tr[0][0])
+#newx11 = testprecomputing(None,b1,b2,x0_0_tr[0][0],x1_0_tr[0][0],x2_0_tr[0][0])
+# le precomputing ne redonne pas le meme resultat : c'est parce qu'ils utilisent la version normalisees source une reponse de l'auteur a ma question
+#print("nos resultats ",newx11)
+#print("leurs resultats ", x1_1_tr[0][0])
 
 # On ne peut pas utiliser de custom dataset 
 class CustomDset(Dataset):
@@ -309,6 +317,7 @@ def train_epoch(train_data, labels, model, loss_fn, optim, device = None, num_cl
     acc = tm.classification.BinaryAccuracy().to(device)
 
     for e in splitted:
+        #print(len(splitted))
         yhat = torch.Tensor([]).to(device)
         # On doit recuperer pour un element du batch les Xk_t
         for b in e:
@@ -385,16 +394,113 @@ def valida_epoch(valid_data, labels, model, loss_fn, device = None, num_classes 
 
     return np.array(allLoss).mean(), acc.compute().item()
 
-def run(model, tdata, tlabels, vdata, val_labels, optim, loss_fn = nn.BCELoss(), num_epoch = 100):
+def test_valide(valid_data, labels, model, loss_fn, device = None, num_classes = 2):
+    if device == None: 
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.eval()
+    # les loss
+    allLoss = []
+    # le roc_auc
+    allroc = []
+
+    labels = torch.Tensor(labels).type(torch.FloatTensor).to(device)
+
+    # Recuperation des donnees
+    x00tr, x01tr, x02tr, x10tr, x11tr, x12tr, x20tr, x21tr, x22tr = valid_data 
+
+    batches = torch.randperm(len(labels))
+    # on split en batch de 64 puisque dataloader ne marche pas
+    splitted = batches.split(64)
+    acc = tm.classification.BinaryAccuracy().to(device)
+
+    yhat = torch.Tensor([]).to(device)
+    for i in range(len(labels)):
+        # On doit recuperer pour un element du batch les Xk_t
+        x0_0 = x00tr[i]
+        x0_1 = x01tr[i]
+        x0_2 = x02tr[i]
+        x1_0 = x10tr[i]
+        x1_1 = x11tr[i]
+        x1_2 = x12tr[i]
+        x2_0 = x20tr[i]
+        x2_1 = x21tr[i]
+        x2_2 = x22tr[i]
+        # Predict de l'element du batch
+        yhat = torch.cat((yhat, model(torch.tensor(x0_0).type(torch.FloatTensor).to(device),torch.tensor(x0_1).type(torch.FloatTensor).to(device),
+		  	torch.tensor(x0_2).type(torch.FloatTensor).to(device),torch.Tensor(x1_0).type(torch.FloatTensor).to(device),torch.Tensor(x1_1).type(torch.FloatTensor).to(device),
+		  	torch.Tensor(x1_2).type(torch.FloatTensor).to(device),torch.Tensor(x2_0).type(torch.FloatTensor).to(device),torch.Tensor(x2_1).type(torch.FloatTensor).to(device),
+		  	torch.Tensor(x2_2).type(torch.FloatTensor).to(device))), 0)
+        
+    yhats = torch.where(yhat > 0.5, 1, 0)
+    #print(f'yhat is {yhat.size()} and labels is {labels[e].size()}')
+    loss = loss_fn(torch.squeeze(yhat).type(torch.FloatTensor).to(device), labels)
+    acc(yhats, labels)
+
+    fpr, tpr, _ = roc_curve(torch.Tensor(labels).type(torch.FloatTensor), torch.squeeze(yhat).type(torch.FloatTensor).detach().numpy())
+
+    cm = confusion_matrix(torch.Tensor(yhats).type(torch.FloatTensor), torch.Tensor(labels).type(torch.FloatTensor))
+    #print(cm)
+
+    return loss.item(), acc.compute().item(), fpr, tpr, cm 
+
+
+def run(model, tdata, tlabels, vdata, val_labels, testdata, testlabels, optim, loss_fn = nn.BCELoss(), num_epoch = 100):
+    epochtrainloss = []
+    epochtrainperfs = []
+    epochvalidloss = []
+    epochvalidperfs = []
     for epoch in tqdm(np.arange(num_epoch)):
         trainloss, trainacc = train_epoch(tdata, tlabels, model,loss_fn, optim)
         validloss, validacc = valida_epoch(vdata,val_labels,model, loss_fn)
         print(f'\nLoss train {trainloss} and accuracy {trainacc}\n')
         print(f'\nLoss validation {validloss} and accuracy {validacc}\n')
+        epochtrainloss.append(trainloss)
+        epochvalidloss.append(validloss)
+        epochtrainperfs.append(trainacc)
+        epochvalidperfs.append(validacc)
+    
+    testloss, testacc, fpr, tpr, cm = test_valide(testdata, testlabels, model, loss_fn)
+
+    print(f'The final loss for test is {testloss} its accuracy is {testacc}')
+
+    plt.subplot(2,2,1)
+    plt.plot(np.array(epochtrainloss))
+    plt.title("loss train")
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    
+    plt.subplot(2,2,2)
+    plt.plot(np.array(epochtrainperfs))
+    plt.title("performances train")
+    plt.xlabel("epoch")
+    plt.ylabel("performances")
+    
+    plt.subplot(2,2,3)
+    plt.plot(np.array(epochvalidloss))
+    plt.title("loss validation")
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+
+    plt.subplot(2,2,4)
+    plt.plot(np.array(epochvalidperfs))
+    plt.title("performances validation")
+    plt.xlabel("epoch")
+    plt.ylabel("performances")
+
+    plt.tight_layout()
+    plt.show()
+
+    roc_display = RocCurveDisplay(fpr=fpr, tpr=tpr).plot()
+    plt.title("courbe roc pour les tests")
+    plt.show()
+
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm).plot()
+    plt.title("Matrice de confusion test")
+    plt.show()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 lr = 0.001
-model = Model(d1=3,d2=2*32,d3=2*32,d4=2*32,d5=2*32,d6=2*32,d7=2*32,d8=2*32,n_c=1).to(device)
+model = Model(d1=3,d2=2*32,d3=2*32,d4=2*32,n_c=1).to(device)
 optim = torch.optim.Adam(list(model.parameters()),lr = lr)
 optim.zero_grad()
 
@@ -402,85 +508,138 @@ optim.zero_grad()
 On concatene toutes les donnees, ils avaient prevu une cross validation 10-folds 
 mais, ils ont deja split le dataset en 3 datasets (train, validation, test) donc on n'en a probablement pas besoin
 """
-# allx00tr = np.array([])
-# for i in range(len(x0_0_tr)):
-#     allx00tr = np.concatenate((allx00tr,x0_0_tr[i]))
 
-# allx01tr = np.array([])
-# for i in range(len(x0_1_tr)):
-#     allx01tr = np.concatenate((allx01tr,x0_1_tr[i]))
+limit = 3
+allx00tr = np.array([])
+for i in range(limit):
+    allx00tr = np.concatenate((allx00tr,x0_0_tr[i]))
 
-# allx02tr = np.array([])
-# for i in range(len(x0_2_tr)):
-#     allx02tr = np.concatenate((allx02tr,x0_2_tr[i]))
+allx01tr = np.array([])
+for i in range(limit):
+    allx01tr = np.concatenate((allx01tr,x0_1_tr[i]))
 
-# allx10tr = np.array([])
-# for i in range(len(x1_0_tr)):
-#     allx10tr = np.concatenate((allx10tr,x1_0_tr[i]))
+allx02tr = np.array([])
+for i in range(limit):
+    allx02tr = np.concatenate((allx02tr,x0_2_tr[i]))
 
-# allx11tr = np.array([])
-# for i in range(len(x1_1_tr)):
-#     allx11tr = np.concatenate((allx11tr,x1_1_tr[i]))
+allx10tr = np.array([])
+for i in range(limit):
+    allx10tr = np.concatenate((allx10tr,x1_0_tr[i]))
 
-# allx12tr = np.array([])
-# for i in range(len(x1_2_tr)):
-#     allx12tr = np.concatenate((allx12tr,x1_2_tr[i]))
+allx11tr = np.array([])
+for i in range(limit):
+    allx11tr = np.concatenate((allx11tr,x1_1_tr[i]))
 
-# allx20tr = np.array([])
-# for i in range(len(x2_0_tr)):
-#     allx20tr = np.concatenate((allx20tr,x2_0_tr[i]))
+allx12tr = np.array([])
+for i in range(limit):
+    allx12tr = np.concatenate((allx12tr,x1_2_tr[i]))
 
-# allx21tr = np.array([])
-# for i in range(len(x2_1_tr)):
-#     allx21tr = np.concatenate((allx21tr,x2_1_tr[i]))
+allx20tr = np.array([])
+for i in range(limit):
+    allx20tr = np.concatenate((allx20tr,x2_0_tr[i]))
 
-# allx22tr = np.array([])
-# for i in range(len(x2_2_tr)):
-#     allx22tr = np.concatenate((allx22tr,x2_2_tr[i]))
+allx21tr = np.array([])
+for i in range(limit):
+    allx21tr = np.concatenate((allx21tr,x2_1_tr[i]))
 
 
-# allx00val = np.array([])
-# for i in range(len(x0_0_val)):
-#     allx00val = np.concatenate((allx00val,x0_0_val[i]))
+alllabstr = np.array([])
+allx22tr = np.array([])
+for i in range(limit):
+    allx22tr = np.concatenate((allx22tr,x2_2_tr[i]))
+    alllabstr = np.hstack((alllabstr,training_labels[i]))
 
-# allx01val = np.array([])
-# for i in range(len(x0_1_val)):
-#     allx01val = np.concatenate((allx01val,x0_1_val[i]))
+# validations
+allx00val = np.array([])
+for i in range(limit):
+    allx00val = np.concatenate((allx00val,x0_0_val[i]))
 
-# allx02val = np.array([])
-# for i in range(len(x0_2_tr)):
-#     allx02val = np.concatenate((allx02val,x0_2_val[i]))
+allx01val = np.array([])
+for i in range(limit):
+    allx01val = np.concatenate((allx01val,x0_1_val[i]))
 
-# allx10val = np.array([])
-# for i in range(len(x1_0_val)):
-#     allx10val = np.concatenate((allx10val,x1_0_val[i]))
+allx02val = np.array([])
+for i in range(limit):
+    allx02val = np.concatenate((allx02val,x0_2_val[i]))
 
-# allx11val = np.array([])
-# for i in range(len(x1_1_val)):
-#     allx11val = np.concatenate((allx11val,x1_1_val[i]))
+allx10val = np.array([])
+for i in range(limit):
+    allx10val = np.concatenate((allx10val,x1_0_val[i]))
 
-# allx12val = np.array([])
-# for i in range(len(x1_2_val)):
-#     allx12val = np.concatenate((allx12val,x1_2_val[i]))
+allx11val = np.array([])
+for i in range(limit):
+    allx11val = np.concatenate((allx11val,x1_1_val[i]))
 
-# allx20val = np.array([])
-# for i in range(len(x2_0_val)):
-#     allx20val = np.concatenate((allx20val,x2_0_val[i]))
+allx12val = np.array([])
+for i in range(limit):
+    allx12val = np.concatenate((allx12val,x1_2_val[i]))
 
-# allx21val = np.array([])
-# for i in range(len(x2_1_val)):
-#     allx21val = np.concatenate((allx21val,x2_1_val[i]))
+allx20val = np.array([])
+for i in range(limit):
+    allx20val = np.concatenate((allx20val,x2_0_val[i]))
 
-# allx22val = np.array([])
-# for i in range(len(x2_2_tr)):
-#     allx22val = np.concatenate((allx22val,x2_2_val[i]))
+allx21val = np.array([])
+for i in range(limit):
+    allx21val = np.concatenate((allx21val,x2_1_val[i]))
 
+alllabsval = np.array([])
+allx22val = np.array([])
+for i in range(limit):
+    allx22val = np.concatenate((allx22val,x2_2_val[i]))
+    alllabsval = np.hstack((alllabsval,val_labels[i]))
+
+# tests
+allx00test = np.array([])
+for i in range(limit):
+    allx00test = np.concatenate((allx00test,x0_0_test[i]))
+
+allx01test = np.array([])
+for i in range(limit):
+    allx01test = np.concatenate((allx01test,x0_1_test[i]))
+
+allx02test = np.array([])
+for i in range(limit):
+    allx02test = np.concatenate((allx02test,x0_2_test[i]))
+
+allx10test = np.array([])
+for i in range(limit):
+    allx10test = np.concatenate((allx10test,x1_0_test[i]))
+
+allx11test = np.array([])
+for i in range(limit):
+    allx11test = np.concatenate((allx11test,x1_1_test[i]))
+
+allx12test = np.array([])
+for i in range(limit):
+    allx12test = np.concatenate((allx12test,x1_2_test[i]))
+
+allx20test = np.array([])
+for i in range(limit):
+    allx20test = np.concatenate((allx20test,x2_0_test[i]))
+
+allx21test = np.array([])
+for i in range(limit):
+    allx21test = np.concatenate((allx21test,x2_1_test[i]))
+
+alllabstest = np.array([])
+allx22test = np.array([])
+for i in range(limit):
+    allx22test = np.concatenate((allx22test,x2_2_test[i]))
+    alllabstest = np.hstack((alllabstest,testing_labels[0][i]))
+
+# print(alllabstr.shape)
+# print(alllabstest.shape)
+
+# Nk x Nk
+# N x Nk x Dk
+# nik, ij -> nik 
 # concatener
-# indata = (x0_0_tr[0], x0_1_tr[0], x0_2_tr[0], x1_0_tr[0], x1_1_tr[0], x1_2_tr[0], x2_0_tr[0], x2_1_tr[0], x2_2_tr[0])
-# indata = (allx00tr, allx01tr, allx02tr, allx10tr, allx11tr, allx12tr, allx20tr, allx21tr, allx22tr)
-# #valdata = (x0_0_val[0], x0_1_val[0], x0_2_val[0], x1_0_val[0], x1_1_val[0], x1_2_val[0], x2_0_val[0], x2_1_val[0], x2_2_val[0])
-# valdata = (allx00val, allx01val, allx02val, allx10val, allx11val, allx12val, allx20val, allx21val, allx22val)
-# run(model, indata ,training_labels[0], valdata, val_labels[0], optim)
+#indata = (x0_0_tr[0], x0_1_tr[0], x0_2_tr[0], x1_0_tr[0], x1_1_tr[0], x1_2_tr[0], x2_0_tr[0], x2_1_tr[0], x2_2_tr[0])
+indata = (allx00tr, allx01tr, allx02tr, allx10tr, allx11tr, allx12tr, allx20tr, allx21tr, allx22tr)
+#valdata = (x0_0_val[0], x0_1_val[0], x0_2_val[0], x1_0_val[0], x1_1_val[0], x1_2_val[0], x2_0_val[0], x2_1_val[0], x2_2_val[0])
+valdata = (allx00val, allx01val, allx02val, allx10val, allx11val, allx12val, allx20val, allx21val, allx22val)
+testdata = (allx00test, allx01test, allx02test, allx10test, allx11test, allx12test, allx20test, allx21test, allx22test)
+run(model, indata ,alllabstr, valdata, alllabsval, testdata, alllabstest, optim)
 #print(len(training_labels[0]))
 
 # sur le fold 0
